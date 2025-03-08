99503249d98b00ae0ac1c2db5790052105429573
FelipeeGeise <felipe19852024@hotmail.com>
2025-03-08 14:05:24 -0300

    Feat: create theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import { StrictMode } from 'react'
+import { ThemeProvider } from 'styled-components'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { darckTheme, GlobalStyle, lighTheme } from './style/'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={darckTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </StrictMode>
 )
